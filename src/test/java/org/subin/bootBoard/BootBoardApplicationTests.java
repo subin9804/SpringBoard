b1fc2f3328a0fa007f8aea79627d2780743b2584
subin9804 <subin9804@naver.com>
2023-05-17 01:06:49 +0900

    스프링 소스 업로드

---
+++ src/test/java/org/subin/bootBoard/BootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.subin.bootBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
