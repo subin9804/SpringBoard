71a9f43e346786a03300e27aa288571cdf9fbfb3
subin9804 <subin9804@naver.com>
2023-05-18 02:16:21 +0900

    회원가입 엔티티 작성

--- src/main/java/org/subin/bootBoard/configs/MvcConfig.java
+++ src/main/java/org/subin/bootBoard/configs/MvcConfig.java
@@ -3,6 +3,7 @@ package org.subin.bootBoard.configs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,6 +12,7 @@ import org.subin.bootBoard.configs.interceptors.SiteConfigInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableJpaAuditing
 public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.path}")
---
+++ src/main/java/org/subin/bootBoard/entities/Member.java
@@ -0,0 +1,37 @@
+package org.subin.bootBoard.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Builder @Data
+@NoArgsConstructor @AllArgsConstructor
+@Table(indexes = {
+        @Index(name = "idx_member_userNm", columnList = "userNm"),
+        @Index(name = "idx_member_email", columnList = "email")
+})
+
+public class Member extends BaseEntity{
+    @Id @GeneratedValue
+    private Long userNo;
+
+    @Column(length = 40, nullable = false, unique = true)
+    private String userId;
+
+    @Column(length = 65, nullable = false)
+    private String userPw;
+
+    @Column(length = 40, nullable = false)
+    private String userNm;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 11)
+    private String mobile;
+
+    @Lob
+    private String termsAgree;  // 약관 동의 내역
+}
