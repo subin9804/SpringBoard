e7f7a75a74863a4b301095f1b09cda9e3c44648b
subin9804 <subin9804@naver.com>
2023-05-20 17:53:20 +0900

    비밀번호 복잡성 세분화 구현

--- src/main/java/org/subin/bootBoard/controllers/members/JoinValidator.java
+++ src/main/java/org/subin/bootBoard/controllers/members/JoinValidator.java
@@ -26,7 +26,7 @@ public class JoinValidator implements Validator, MobileValidator {
          * 2. 비밀번호 복잡성 체크(알파벳(대문자, 소문자), 숫자, 특수문자))
          * 3. 비밀번호와 비밀번호 확인 일치
          * 4. 휴대전화번호(선택) - 입력된 경우 형식 체크
-         * 5. 휴대전화번호가 입력된 경우 숫자만 추출해서
+         * 5. 휴대전화번호가 입력된 경우 숫자만 추출해서 다시 커맨드 객체에 저장
          * 6. 필수 약관 동의 체크
          */
 
@@ -34,6 +34,9 @@ public class JoinValidator implements Validator, MobileValidator {
         String userId = joinForm.getUserId();
         String userPw = joinForm.getUserPw();
         String userPwRe = joinForm.getUserPwRe();
+        String mobile = joinForm.getMobile();
+        boolean[] agrees = joinForm.getAgrees();    // 필수 약관
+
 
         // 1. 아이디 중복 여부
         if(userId != null && !userId.isBlank() && memberRepository.exists(userId)) {
@@ -42,14 +45,33 @@ public class JoinValidator implements Validator, MobileValidator {
 
         // 2. 비밀번호 복잡성 체크(알파벳(대문자, 소문자), 숫자, 특수문자))
 
+
+
+
         // 3. 비밀번호와 비밀번호 확인 일치
         if(userPw != null && !userPw.isBlank() && userPwRe != null && !userPwRe.isBlank() && !userPw.equals(userPwRe)){
             errors.rejectValue("userPwRe", "Validation.incorrect.userPwRe");
         }
 
         // 4. 휴대전화번호(선택) - 입력된 경우 형식 체크
+        // 5. 휴대전화번호가 입력된 경우 숫자만 추출해서 다시 커맨드 객체에 저장
+        if(mobile != null && !mobile.isBlank()) {
+            if(!mobileNumCheck(mobile)) {
+                errors.rejectValue("mobile", "Validation.mobile");
+            }
+            mobile = mobile.replaceAll("\\D", "");
+            joinForm.setMobile(mobile);
+        }
 
-
+        // 6. 필수 약관 동의 체크
+        if(agrees != null && agrees.length > 0) {
+            for(boolean agree : agrees) {
+                if(!agree) {
+                    errors.reject("Validation.joinForm.agree");
+                    break;
+                }
+            }
+        }
 
     }
 }
