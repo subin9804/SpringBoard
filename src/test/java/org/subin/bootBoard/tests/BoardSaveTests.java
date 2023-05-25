b65cd74b7340ead453897432e4ebce0005de6c6d
subin9804 <subin9804@naver.com>
2023-05-26 01:32:07 +0900

    게시판 작성 양식 완료

---
+++ src/test/java/org/subin/bootBoard/tests/BoardSaveTests.java
@@ -0,0 +1,11 @@
+package org.subin.bootBoard.tests;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class BoardSaveTests {
+}
