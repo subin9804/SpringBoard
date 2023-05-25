2014f56834d79f39ed0269d00b73a424b59c81df
subin9804 <subin9804@naver.com>
2023-05-25 15:41:57 +0900

    게시판 목록 완료 , 프론트 컨트롤러 구성 중

---
+++ src/main/java/org/subin/bootBoard/controllers/boards/BoardController.java
@@ -0,0 +1,51 @@
+package org.subin.bootBoard.controllers.boards;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    /**
+     * 게시글 목록
+     * @param bId
+     * @return
+     */
+    @GetMapping("/list/{bId}")
+    public String list(@PathVariable String bId) {
+
+        return "board/list";
+    }
+
+    /**
+     * 게시글 작성
+     * @param bId
+     * @return
+     */
+    @GetMapping("/write/{bId}")
+    public String write(String bId) {
+
+        return "board/write";
+    }
+
+
+    /**
+     * 게시글 수정
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}/update")
+    public String update(@PathVariable Long id) {
+
+        return "board/update";
+    }
+
+    @GetMapping("view/{id}")
+    public String view(@PathVariable Long id) {
+
+        return "board/view";
+    }
+}
