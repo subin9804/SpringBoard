9c956b56f3b82b8e5133830c44e8424edc40c0b1
subin9804 <subin9804@naver.com>
2023-08-25 17:40:17 +0900

    fileController

--- src/main/java/org/subin/bootBoard/controllers/test/TestController.java
+++ src/main/java/org/subin/bootBoard/controllers/test/TestController.java
@@ -1,13 +1,18 @@
 package org.subin.bootBoard.controllers.test;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.subin.bootBoard.commons.rests.JSONData;
+import org.subin.bootBoard.entities.FileInfo;
 import org.subin.bootBoard.models.file.FileUploadService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,9 +27,10 @@ public class TestController {
         return "test";
     }
 
+
     @PostMapping
     @ResponseBody
     public void uploadPs(MultipartFile[] files,String gid, String location)  {
-        uploadService.upload(files, gid, location);
+
     }
 }
--- src/main/java/org/subin/bootBoard/restcontrollers/file/FileController.java
+++ src/main/java/org/subin/bootBoard/restcontrollers/file/FileController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.subin.bootBoard.commons.rests.JSONData;
 import org.subin.bootBoard.entities.FileInfo;
+import org.subin.bootBoard.models.file.FileDeleteService;
 import org.subin.bootBoard.models.file.FileDownloadService;
 import org.subin.bootBoard.models.file.FileUploadService;
 
@@ -20,9 +21,38 @@ public class FileController {
 
     private final FileUploadService uploadService;
     private final FileDownloadService downloadService;
+    private final FileDeleteService deleteService;
 
+    /**
+     * 파일 업로드 처리
+     * @param files
+     * @param gid
+     * @param location
+     */
     @PostMapping("/upload")
     public ResponseEntity<JSONData<List<FileInfo>>> uploadPs(MultipartFile[] files, String gid, String location) {
+        List<FileInfo> items = uploadService.upload(files, gid, location);
 
+        JSONData<List<FileInfo>> data = new JSONData<>();
+        data.setSuccess(true);
+        data.setData(items);
+
+        return ResponseEntity.ok(data);
+    }
+
+    @RequestMapping("/download/{id}")
+    public void download(Long id) {
+        downloadService.download(id);
+    }
+
+    @RequestMapping("/delete/{id}")
+    public ResponseEntity<JSONData<Long>> delete(Long id) {
+        deleteService.delete(id);
+
+        JSONData<Long> data = new JSONData<>();
+        data.setSuccess(true);
+        data.setData(id);
+
+        return ResponseEntity.ok(data);
     }
 }
